84df18327109339a7bbba58dfa5838a4082335c6
Tony Bautista <jubautista@chapman.edu>
2020-11-14 20:32:01 -0800

    fixed more coding issues

--- username.sh
+++ username.sh
@@ -5,7 +5,7 @@ echo " Please create a username using lowerecase letters, numbers and an undersc
 echo "The username must start with a lowerecase letter and between 3-12 characters."
 echo "Enter a username: "
 read USERNAME
-while echo "$USERNAME" | egrep -v "^[a-z] [0-9] [_] {3,12}$" > /dev/null 2>&1
+while echo "$USERNAME" | egrep -v "^[a-z][a-z0-9_]{2,11}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username  - only lowercase letters, digits and underscore can be used, must contain at least three characters and no more than 12 characters!"
 	echo "Enter a username: "
