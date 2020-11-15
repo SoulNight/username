83f528f23917d6f06d14d2b75c6d71482714bb06
Tony Bautista <jubautista@chapman.edu>
2020-11-14 19:42:07 -0800

    made updates to code

--- username.sh
+++ username.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 # username.sh 
 # Tony Bautista 
-echo "Enter a five-digit ZIP code: "
-read ZIP
-while echo "$ZIP" | egrep -v "^[a-z] [0-9] {5}$" > /dev/null 2>&1
+echo " Please create a username using lowerecase letters, numbers and an underscore."
+echo "The username must start with a lowerecase letter and between 3-12 charachters."
+echo "Enter a username: "
+read USERNAME
+while echo "$USERNAME" | egrep -v "^[a-z] [0-9,] [_] {3-12}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid ZIP code - five digits only!"
-	echo "Enter a five-digit ZIP code: "
-	read ZIP
+	echo "You must enter a valid username  - only lowercase letters, digits and underscore can be used, must contain at least three characters and no more than 12 characters!"
+	echo "Enter a username: "
+	read USERNAME
 done
-echo "Thank you"
+echo "Thank you, your username is valid."
